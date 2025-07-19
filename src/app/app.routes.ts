e0a4175797f7d1fb254830f4a444c511b405eefe
dhonataborges <dhonata.borges2017@gmail.com>
2025-07-18 23:04:10 -0300

    Subindo projeto Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,15 @@ import { LoginComponent } from './pages/login/login.component';
 import { UsuarioCreateComponent } from './pages/usuario/usuario-create/usuario-create.component';
 import { authGuard } from './auth/auth.guard';
 import { UsuarioUpdateComponent } from './pages/usuario/usuario-update/usuario-update.component';
+import { LoadingComponent } from './pages/loading/loading.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'usuario-create', component: UsuarioCreateComponent},
+  
+  { path: 'teste', component: LoadingComponent},
   {
     path: 'bebida-create',
     component: BebidaCreateComponent,
